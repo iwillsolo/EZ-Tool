e3030fdb65f9e9b197fa94c07c2ae1e946d50560
Abdalrahman Gamal <abdalrahman.gamal.m@gmail.com>
2022-01-23 09:10:45 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('#myInput').on('keyup',function(e){
+    // cache all the tag elements in an array
+    var tagElems = $('#myUL').children();
+  
+    // hide all tags
+    $(tagElems).hide();
+  
+  
+    for(var i = 0; i < tagElems.length; i++){
+        console.log(i); // loop through all tagElements
+      var tag = $(tagElems).eq(i);
+  
+      if(($(tag).text().toLowerCase()).indexOf($(this).val().toLowerCase()) === 0){
+        // if element's text value starts with the hint show that tag element
+        $(tag).show();
+      }
+    }
+  });
