e220881ff97dc8554b0a67b85b736504731f4e7a
Abdalrahman Gamal <abdalrahman.gamal.m@gmail.com>
2022-01-31 12:40:57 +0200

    H188A added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 $('#myInput').on('keyup',function(e){
-    // cache all the tag elements in an array
-    var tagElems = $('#myUL').children();
-  
-    // hide all tags
-    $(tagElems).hide();
-  
-  
-    for(var i = 0; i < tagElems.length; i++){
-        console.log(i); // loop through all tagElements
-      var tag = $(tagElems).eq(i);
-  
-      if(($(tag).text().toLowerCase()).indexOf($(this).val().toLowerCase()) === 0){
-        // if element's text value starts with the hint show that tag element
-        $(tag).show();
-      }
+  // cache all the tag elements in an array
+  var tagElems = $('#myUL').children();
+
+  // hide all tags
+  $(tagElems).hide();
+
+  for(var i = 0; i < tagElems.length; i++){
+      // loop through all tagElements
+    var tag = $(tagElems).eq(i);
+
+    if(($(tag).text().toLowerCase()).indexOf($(this).val().toLowerCase()) === 0 ){
+      // if element's text value starts with the hint show that tag element
+      $(tag).show();
     }
-  });
+  }
+});
